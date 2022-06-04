d328625a662775cfbd15cd6df6deb49f13900efc
Tan Tiong Guan <tantiongguan123@gmail.com>
2022-06-04 20:59:13 +0800

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+import HomePage from './Pages/HomePage/HomePage';
+
+const theme = createTheme({
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <HomePage />
+        </ThemeProvider>
+    );
 }
 
 export default App;
